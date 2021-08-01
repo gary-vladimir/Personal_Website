5c34436e3213bda43dbc608bb62f3611ed79b042
gary-vladimir <gary@garybricks.com>
2021-08-01 18:11:58 -0500

    navBar styles for mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,29 @@
+import React from 'react';
+
 function NavBar() {
     return (
         <div className="navbar">
-            <div className="burger">
+            <div
+                className="burger"
+                onClick={() => {
+                    console.log('user pressed menu');
+                }}
+            >
                 <i className="fas fa-bars"></i>
             </div>
             <div href="#home" className="item">
                 Home
             </div>
-            <div href="#home" className="item">
+            <div href="#about" className="item">
                 About
             </div>
-            <div href="#home" className="item">
+            <div href="#work" className="item">
                 Work
             </div>
-            <div href="#home" className="item">
+            <div href="#blog" className="item">
                 Blog
             </div>
-            <div href="#home" className="item">
+            <div href="#contact" className="item">
                 Contact
             </div>
         </div>
